ffb13bca3ef0cc211be5b63da3223d43dd97b1d3
RamazanAkdag <akdagramazan586@gmail.com>
2024-06-24 13:47:24 +0300

    database properties added

---
+++ src/main/java/com/id3/day_1_rest_service/Day1RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.id3.day_1_rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1RestServiceApplication.class, args);
+	}
+
+}
